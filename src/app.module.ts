5519b9e3cd5936e0fc540338177a05cc8e7564f9
leey00nsu <dbstndla1212@naver.com>
2024-06-28 02:31:15 +0900

    refactor. typeorm 사용을 위한 데이터베이스 설정 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleLimitService } from './article/article-limit.service';
 import { ArticleModule } from './article/article.module';
 import { AuthModule } from './auth/auth.module';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RedisModule } from './redis/redis.module';
 import { UsersModule } from './user/user.module';
 
@@ -15,15 +16,18 @@ import { UsersModule } from './user/user.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: 'syu-character-maker',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      // synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: +configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: 'syu-character-maker',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
     }),
     AuthModule,
     UsersModule,
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ import { RedisModule } from './redis/redis.module';
 import { RedisService } from './redis/redis.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug'],
+  });
 
   const redisService = app.select(RedisModule).get(RedisService);
 
