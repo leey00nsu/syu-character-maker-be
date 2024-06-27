6db907f8fe22db567a80550457415fb694b7bd6f
leey00nsu <dbstndla1212@naver.com>
2024-06-28 00:49:30 +0900

    refactor. redis 패스워드 추가

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -6,10 +6,9 @@ export class RedisService {
   private readonly redis: Redis;
 
   constructor() {
-    this.redis = new Redis({
-      host: process.env.REDIS_HOST,
-      port: Number(process.env.REDIS_PORT),
-    });
+    this.redis = new Redis(
+      `redis://:${process.env.REDIS_PASSWORD}@w8wskg4:6379/0`,
+    );
   }
 
   async getClient() {
